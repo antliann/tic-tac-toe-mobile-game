ffa63f195ffd459df3574215ba2678490b8fa74b
antliann <antonliann@gmail.com>
2021-09-16 09:30:08 +0300

    settings bar styling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,13 +3,14 @@ import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
 import {Dimensions} from 'react-native';
 
 import {Board} from "./Board";
+import {SettingsBar} from "./SettingsBar";
 
 import {calculateWinner, botMind} from "../methods";
 
 import X from '../icons/x.svg';
 import O from '../icons/o.svg';
 
-const MOVE_BUTTON_WIDTH = 0.9 * Dimensions.get('window').width / 5 - 16;
+const MOVE_BUTTON_WIDTH = 0.9 * Dimensions.get('window').width / 6 - 15;
 const BOT_MOVE_TIMEOUT = 600;
 
 class Game extends React.Component {
@@ -28,7 +29,12 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    if (!this.state.gameHasStarted) this.setState({gameHasStarted: true});
+    if ((
+      !this.state.gameHasStarted && !this.state.firstIsO) || (
+      this.state.firstIsO && this.state.stepNumber === 1)
+    ) {
+      this.setState({gameHasStarted: true});
+    }
 
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
@@ -56,16 +62,17 @@ class Game extends React.Component {
       setTimeout(() => this.botMove(), BOT_MOVE_TIMEOUT);
   }
 
-  jumpTo(step) {
+  jumpTo(step, withReset = false) {
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2) === 0,
     });
-    if (!step) {
+    if (withReset) {
       this.setState({
         history: [{
           squares: Array(9).fill(null),
         }],
+        gameHasStarted: false,
       }, () => {
         if (this.state.firstIsO) this.botMove()
       });
@@ -73,36 +80,41 @@ class Game extends React.Component {
   }
 
   defineStatus(winner) {
-    return winner ?
-      ((winner === 'X') === this.state.firstIsO) ?
-        <Text style={styles.redText}>You lose</Text> :
-        <Text style={styles.greenText}>You won</Text> :
+    return winner ? (
+        <View style={[styles.row, styles.statusTextContainer]}>
+          {((winner === 'X') === this.state.firstIsO) ?
+            <Text style={styles.redText}>You lose</Text> :
+            <Text style={styles.greenText}>You won</Text>
+          }
+        </View>
+      ) : (
       this.state.stepNumber < 9 ? (
-          <View style={styles.row}>
-            <Text style={styles.commonText}>Current player:</Text>
-            <View>{this.state.xIsNext ? <X width={30}/> : <O width={30}/>}</View>
+          <View style={[styles.row, styles.alignCenter]}>
+            <View style={styles.statusTextContainer}>
+              <Text style={styles.commonText}>Current player:</Text>
+            </View>
+            <View>{this.state.xIsNext ? <X width={26} height={26}/> : <O width={26} height={26}/>}</View>
           </View>
         ) :
-        <Text>Draw</Text>;
-  }
+        <Text style={styles.blueText}>Draw</Text>
+  )}
 
   getMoveButtonStyle(isSelected) {
     return {
       width: MOVE_BUTTON_WIDTH,
-      height: 50,
+      height: 46,
       marginTop: 8,
       marginRight: 10,
       backgroundColor: '#000f44',
-      borderRadius: 5,
+      borderRadius: 8,
       borderWidth: isSelected ? 2 : 0,
       borderColor: '#4b74ff',
-      paddingTop: isSelected ? 6 : 8,
+      paddingTop: isSelected ? 9 : 11,
     }
   }
 
   renderMovesButtons() {
-    console.log(this.state.gameHasStarted);
-    if (!this.state.gameHasStarted) return <View height={58}/>
+    if (!this.state.gameHasStarted) return <View height={54}/>
 
     return this.state.history.map(
       (step, move) => {
@@ -141,10 +153,26 @@ class Game extends React.Component {
             onCellPress={(i) => this.userClick(i)}
           />
         </View>
-        <View style={styles.movesBarContainer}>
+        <View style={styles.movesBar}>
           <Text style={styles.commonText}>Go to the move:</Text>
           <View style={styles.row}>{this.renderMovesButtons()}</View>
         </View>
+        <SettingsBar
+          changeFirst={() => {
+            this.setState({firstIsO: !this.state.firstIsO});
+            this.jumpTo(0, true);
+          }}
+          changeMode={() => {
+            this.setState({hardMode: !this.state.hardMode});
+            this.jumpTo(0, true);
+          }}
+          firstIsO={this.state.firstIsO}
+          hardMode={this.state.hardMode}
+          newGame={() => {
+            this.jumpTo(0, true)
+          }}
+          gameHasStarted={this.state.gameHasStarted}
+        />
         <View style={styles.statusContainer}>
           {this.defineStatus(winner)}
         </View>
@@ -169,7 +197,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  movesBarContainer: {
+  movesBar: {
     position: "absolute",
     top: 50,
     backgroundColor: '#001250',
@@ -179,7 +207,7 @@ const styles = StyleSheet.create({
   },
   statusContainer: {
     position: "absolute",
-    bottom: 200,
+    top: 205,
   },
   row: {
     flexDirection: 'row',
@@ -193,23 +221,32 @@ const styles = StyleSheet.create({
   redText: {
     fontFamily: 'Manjari',
     color: '#cf0000',
-    fontSize: 26,
+    fontSize: 23,
   },
   greenText: {
     fontFamily: 'Manjari',
     color: '#00b533',
-    fontSize: 26,
+    fontSize: 23,
+  },
+  blueText: {
+    fontFamily: 'Manjari',
+    color: '#4b74ff',
+    fontSize: 23,
   },
   movesText: {
     fontFamily: 'Manjari',
     color: 'white',
-    fontSize: 40,
+    fontSize: 30,
     textAlign: 'center',
   },
   moveContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    height: 40,
-  }
+    height: 30,
+  },
+  statusTextContainer: {
+    marginTop: 7,
+    marginRight: 8,
+  },
 });
--- src/components/SettingsBar.jsx
+++ src/components/SettingsBar.jsx
@@ -3,19 +3,39 @@ import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
 
 import X from '../icons/x.svg';
 import O from '../icons/o.svg';
+import NewGameIcon from '../icons/newGame.svg';
 
 class SettingsBar extends React.Component {
-  getButtonStyle(isSelected) {
+  getPlayAsButtonStyle(isSelected) {
     return {
-      marginTop: 8,
+      marginBottom: 8,
       marginRight: 10,
       backgroundColor: '#000f44',
-      borderRadius: 5,
+      borderRadius: 8,
       borderWidth: isSelected ? 2 : 0,
       borderColor: '#4b74ff',
-      padding: isSelected ? 8 : 10,
+      height: 46,
+      width: 50,
       alignItems: 'center',
+      justifyContent: 'center',
       flexGrow: 0,
+      flex: 0,
+    }
+  }
+
+  getModeButtonStyle(isSelected) {
+    return {
+      marginBottom: 8,
+      marginRight: 10,
+      backgroundColor: '#000f44',
+      borderRadius: 8,
+      borderWidth: isSelected ? 2 : 0,
+      borderColor: '#4b74ff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 80,
+      height: 46,
+      paddingTop: isSelected ? 7 : 9,
     }
   }
 
@@ -24,33 +44,41 @@ class SettingsBar extends React.Component {
       <View style={styles.bar}>
         <View>
           <Text style={styles.commonText}>Play as:</Text>
-          <View style={styles.row}>
+          <View style={styles.alignCenter}>
             <TouchableOpacity
               disabled={!this.props.firstIsO || this.props.gameHasStarted}
               onPress={this.props.changeFirst}
-              style={this.getButtonStyle(!this.props.firstIsO)}>
-              <X width={30}/>
+              style={this.getPlayAsButtonStyle(!this.props.firstIsO)}>
+              <X width={26} height={26}/>
             </TouchableOpacity>
             <TouchableOpacity
               disabled={this.props.firstIsO || this.props.gameHasStarted}
               onPress={this.props.changeFirst}
-              style={this.getButtonStyle(this.props.firstIsO)}>
-              <O width={30}/>
+              style={this.getPlayAsButtonStyle(this.props.firstIsO)}>
+              <O width={26} height={26}/>
             </TouchableOpacity>
           </View>
         </View>
-        <Text>Game mode</Text>
-        <View>
-          <TouchableOpacity disabled={!this.props.hardMode} onPress={this.props.changeMode}>
-            <Text>Easy</Text>
+        <View style={styles.flex}>
+          <Text style={styles.commonText}>Game mode</Text>
+          <TouchableOpacity
+            disabled={!this.props.hardMode}
+            onPress={this.props.changeMode}
+            style={this.getModeButtonStyle(!this.props.hardMode)}>
+            <Text style={styles.commonText}>Easy</Text>
           </TouchableOpacity>
-          <TouchableOpacity disabled={this.props.hardMode} onPress={this.props.changeMode}>
-            <Text>Hard</Text>
+          <TouchableOpacity
+            disabled={this.props.hardMode}
+            onPress={this.props.changeMode}
+            style={this.getModeButtonStyle(this.props.hardMode)}>
+            <Text style={styles.commonText}>Hard</Text>
           </TouchableOpacity>
         </View>
-        <TouchableOpacity onPress={this.props.newGame}>
-          <Text>New game</Text>
-        </TouchableOpacity>
+        <View style={styles.justifyCenter}>
+          <TouchableOpacity onPress={this.props.newGame}>
+            <NewGameIcon />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
@@ -62,21 +90,40 @@ const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: '#001250',
     width: '90%',
-    padding: 20,
+    padding: 16,
     borderTopRightRadius: 10,
     borderTopLeftRadius: 10,
-    height: 150,
+    height: 180,
     flexDirection: 'row',
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
   },
+  flex: {
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignItems: 'center',
+  },
   commonText: {
     fontFamily: 'Manjari',
     color: 'white',
     fontSize: 20,
+    textAlign: "center",
+    padding: 6,
+    paddingBottom: 8,
   },
+  alignCenter: {
+    alignItems: 'center',
+  },
+  justifyCenter: {
+    justifyContent: 'center',
+  },
+  noGrow: {
+    flex: 0,
+    flexGrow: 0
+  }
 });
 
 export {SettingsBar};
